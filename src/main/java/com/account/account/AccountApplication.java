8304f0b04c7bee2e85083527479cf7b598457596
dmendozy <dmendozy@everis.com>
2020-07-21 12:49:55 -0500

    update

--- src/main/java/com/account/account/AccountApplication.java
+++ src/main/java/com/account/account/AccountApplication.java
@@ -3,11 +3,9 @@ package com.account.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import springfox.documentation.swagger2.annotations.EnableSwagger2WebFlux;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableSwagger2WebFlux
 public class AccountApplication {
 
 	public static void main(String[] args) {
