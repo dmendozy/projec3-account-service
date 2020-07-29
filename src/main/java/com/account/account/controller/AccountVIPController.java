cc4d43056e578b059fc84b3a86ad7e9e378e6ec8
dmendozy <dmendozy@everis.com>
2020-07-29 17:13:30 -0500

    update

--- src/main/java/com/account/account/controller/AccountVIPController.java
+++ src/main/java/com/account/account/controller/AccountVIPController.java
@@ -1,5 +1,6 @@
 package com.account.account.controller;
 
+import com.account.account.model.Account;
 import com.account.account.model.AccountVIP;
 import com.account.account.service.AccountVIPService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,6 +8,8 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
+
+import java.time.LocalDate;
 
 @RestController
 @RequestMapping("/accounts/vip")
@@ -37,5 +40,20 @@ public class AccountVIPController {
     @DeleteMapping("{id}")
     public Mono<AccountVIP> deleteAccount(@PathVariable("id") String accountId){
         return accountVIPService.delete(accountId);
+    }
+
+    @GetMapping("/vip/search/{bankId}/{firstDate}/{lastDate}")
+    public Flux<Account> getAccountsBetweenDates(@PathVariable("bankId") String bankId,
+                                                 @PathVariable("firstDate") String firstDate,
+                                                 @PathVariable("lastDate") String lastDate) {
+        LocalDate date1 = LocalDate.parse(firstDate);
+        LocalDate date2 = LocalDate.parse(lastDate);
+        return accountVIPService.getAll()
+                .filter(account -> account.getBankId()!=null&&
+                        account.getCreationDate().compareTo(date1)>=0&&
+                        account.getCreationDate().compareTo(date2)<=0)
+                .flatMap(account -> {
+                    return accountVIPService.getById(account.getAccountId());
+                });
     }
 }
