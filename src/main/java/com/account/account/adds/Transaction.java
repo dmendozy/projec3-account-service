041fe57ca631e08661bb68f52a9d9005d1be8106
dmendozy <dmendozy@everis.com>
2020-07-29 17:13:18 -0500

    add Transaction

--- src/main/java/com/account/account/adds/Transaction.java
+++ src/main/java/com/account/account/adds/Transaction.java
@@ -2,12 +2,16 @@ package com.account.account.adds;
 
 import lombok.Data;
 
+import java.time.LocalDate;
+
 @Data
 public class Transaction {
     public String transactionId;
     public String transactionName;
     public double output;
     public double input;
-    public String datetime;
+    public double commission;
+    public LocalDate datetime;
     public String accountId;
+    public String creditId;
 }
