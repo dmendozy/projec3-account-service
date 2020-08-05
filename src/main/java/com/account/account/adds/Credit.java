e2a08b54eac75e0a0290c1dd2a4d6579ebc9ad56
dmendozy <67970157+dmendozy@users.noreply.github.com>
2020-08-04 21:49:26 -0500

    add credit

---
+++ src/main/java/com/account/account/adds/Credit.java
@@ -0,0 +1,17 @@
+package com.account.account.adds;
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Credit {
+    public String creditId;
+    public String bankId;
+    public String numberCredit;
+    public String typeAccount;
+    public double creditLimit;
+    public LocalDate creationDate;
+    public LocalDate expirationPayment;
+    public String customerId;
+}
